85b94b35a19e56ca2edf178599592a0c728f5eec
gonsalvesjonh <83717279+gonsalvesjonh@users.noreply.github.com>
2021-05-07 14:49:36 -0300

    Add files via upload

---
+++ EasyComCRUD/cadastro-php.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Hello, mundo</title>
+</head>
+<body>
+<div>
+
+<?php
+
+
+    $tNome  =   $_POST["tNome"];
+    $tMail  =   $_POST["tMail"];
+    $tSexo  =   $_POST["tSexo"];
+    $tData  =   $_POST["tData"];
+    $valor = $_POST["tEnv"];
+    echo   "Valor devolvido $valor";
+
+?>
+
+</div>
+</body>
+</html>
---
+++ EasyComCRUD/cadastros.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.2
+ */
+
+/**
+ * Database `gerenciamento_easy`
+ */
+
+/* `gerenciamento_easy`.`cadastros` */
+$cadastros = array(
+);
